dc6744411510b31c3935d32fbf53d75279bbf267
rakhisinha8 <109195063+rakhisinha8@users.noreply.github.com>
2022-07-24 23:13:38 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+string = "Subtraction"
+st.set_page_config(page_title=string, page_icon="🔍")
+
+st.title('Subtraction')
+x = st.number_input('Enter a number')
+Y = st.number_input('Enter another number')
+Z = x-y
+st.write("difference is",z)
+    
